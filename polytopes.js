c50095760942abc62da220af6f88fda5cdb499db
Mike Lynch <mike@mikelynch.org>
2023-07-31 17:24:26 +1000

    Turned off debug for 600-cell

--- polytopes.js
+++ polytopes.js
@@ -186,7 +186,7 @@ function make_600cell_vertices() {
 
 export const cell600 = () => {
 	const nodes  = make_600cell_vertices();
-	const links = auto_detect_edges(nodes, 12, true);
+	const links = auto_detect_edges(nodes, 12);
 	return {
 		nodes: nodes,
 		links: links,
